328b52d0d201a240aae9c5b881601eab50b6d4c0
capJavert <ante.baric3@gmail.com>
2017-12-02 16:32:52 +0100

    2-day part 2 completed

--- 2-day.py
+++ 2-day.py
@@ -18,21 +18,18 @@ def main():
     checksum = 0
 
     for line in spreadsheet.splitlines():
-        max_value = 0
-        min_value = 10000
+        for digit1 in line.split("\t"):
+            digit1 = int(digit1)
 
-        for digit in line.split("\t"):
-            digit = int(digit)
+            for digit2 in line.split("\t"):
+                digit2 = int(digit2)
 
-            if digit < min_value:
-                min_value = digit
+                if digit1 != digit2:
+                    if digit1 % digit2 == 0:
+                        checksum += digit1 / digit2
+                        break
 
-            if digit > max_value:
-                max_value = digit
-
-        checksum += max_value - min_value
-
-    print(checksum)
+    print(int(checksum))
 
 
 main()
