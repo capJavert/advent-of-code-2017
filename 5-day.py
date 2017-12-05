48782b5e75b215865abf7fa8aa0ca5e686b8eb99
capJavert <ante.baric3@gmail.com>
2017-12-05 08:56:15 +0100

    5-day part 1 completed

---
+++ 5-day.py
@@ -0,0 +1,31 @@
+import requests
+
+
+def get_and_prepare_data_string():
+    """
+    fetch data for my user from pastebin
+    """
+
+    request = requests.get("https://pastebin.com/raw/n37ZVwjj")
+    request.encoding = 'ISO-8859-1'
+
+    return request.text
+
+
+def main():
+    instructions = get_and_prepare_data_string().splitlines()
+
+    steps = 0
+    index = 0
+
+    while index < len(instructions):
+        jump = int(instructions[index])
+        instructions[index] = jump+1
+        index += jump
+
+        steps += 1
+
+    print(steps)
+
+
+main()
