8c55b30e08c9fa0033cb6ee0bea8b9bd43edbe84
capJavert <ante.baric3@gmail.com>
2017-12-06 14:45:27 +0100

    6-day part 1 completed

---
+++ 6-day.py
@@ -0,0 +1,46 @@
+import requests
+
+
+def get_and_prepare_data_string():
+    """
+    fetch data for my user from pastebin
+    """
+
+    request = requests.get("https://pastebin.com/raw/DqAj6dGk")
+    request.encoding = 'ISO-8859-1'
+
+    return request.text
+
+
+def main():
+    memory_banks = list(map(int, get_and_prepare_data_string().split("\t")))
+
+    states = [";".join(map(str, memory_banks))]
+    steps = 0
+
+    while True:
+        current = max(memory_banks)
+        index = current_index = memory_banks.index(current)
+        memory_banks[current_index] = 0
+
+        while True:
+            index = (index+1) % len(memory_banks)
+
+            if current == 0:
+                break
+
+            memory_banks[index] += 1
+            current -= 1
+
+        list_id = ";".join(map(str, memory_banks))
+        steps += 1
+
+        if list_id in states:
+            break
+
+        states.append(list_id)
+
+    print(steps)
+
+
+main()
