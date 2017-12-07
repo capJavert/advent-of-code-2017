aa25bc5c5a75de69fb7c8da258c88f75d68ec57d
capJavert <ante.baric3@gmail.com>
2017-12-07 14:21:18 +0100

    7-day part 2 completed

--- 7-day.py
+++ 7-day.py
@@ -1,5 +1,5 @@
-from anytree import Node, RenderTree
 import requests
+from anytree import Node, RenderTree, PreOrderIter
 
 
 def get_and_prepare_data_string():
@@ -14,39 +14,128 @@ def get_and_prepare_data_string():
     return request.text
 
 
-"""def f_children(program_name):
-    for child in tree[program_name]["p"]:
-        Node(child, parent=tree[program_name])
-        if len(program["p"]) > 0:
-            children(pro)"""
+def calculate_balance(element_name, prefix="", visual_mode=False, break_filter=None, parse_filter=None):
+    if parse_filter:
+        if element_name not in parse_filter:
+            return
+
+    if break_filter:
+        if prefix.count("\t") > break_filter:
+            return
+
+    if visual_mode:
+        print(prefix+str(prefix.count("\t"))+". "+element_name+": "+str(weights[element_name]))
+        prefix += "\t"
+
+    for element in tree[element_name]["p"]:
+        calculate_balance(element, prefix, visual_mode=visual_mode, break_filter=break_filter,
+                          parse_filter=parse_filter)
 
 
-tree = {}
+def build_tree(element_name, parent=None, parse_filter=None):
+    if parse_filter:
+        if element_name not in parse_filter:
+            return
+
+    if parent:
+        node_tree[element_name] = Node(element_name, parent=parent)
+    else:
+        node_tree[element_name] = Node(element_name)
+
+    for element in tree[element_name]["p"]:
+        node_tree[element] = Node(element, node_tree[element_name])
+        build_tree(element, node_tree[element], parse_filter=parse_filter)
 
 
-def main():
-    raw = get_and_prepare_data_string().splitlines()
+def check_balance(element_name, parent=None):
+    if element_name not in weights:
+        weights[element_name] = 0
 
+    if parent:
+        if parent not in weights:
+            weights[parent] = 0
+
+    if parent:
+        weights[parent] += tree[element_name]["w"]
+
+    weights[element_name] += tree[element_name]["w"]
+
+    for element in tree[element_name]["p"]:
+        if parent:
+            weights[parent] += tree[element]["w"]
+        else:
+            weights[element_name] += tree[element]["w"]
+
+        check_balance(element, element_name)
+
+
+def parse_tree(raw, parse_filter=None):
     for element in raw:
         element_name = element.split(" ")[0]
+
+        if parse_filter:
+            if element_name not in parse_filter:
+                continue
+
         parents = []
 
         if "->" in element:
             parents = element.split("-> ")[1].split(", ")
 
-        # weight = int(program.split("(")[1].split(")")[0])
+        weight = int(element.split("(")[1].split(")")[0])
 
-        if element_name not in tree:
-            tree[element_name] = Node(element_name)
+        tree[element_name] = {"w": weight, "p": parents}
 
-        for parent in parents:
-            if parent in tree:
-                tree[parent].parent = tree[element_name]
-            else:
-                tree[parent] = Node(parent, parent=tree[element_name])
 
-    for name, element in tree.items():
-        print(RenderTree(element))
+weights = {}
+tree = {}
+node_tree = {}
+
+
+def main():
+    raw = get_and_prepare_data_string().splitlines()
+    bottom = "ykpsek"  # check my solution for part 1
+
+    parse_tree(raw)
+
+    # calculate_balance(bottom)
+
+    # print(weights)
+
+    build_tree(bottom)
+    # print(RenderTree(node_tree["tknk"]))
+
+    check_balance(bottom)
+
+    # calculate_balance(bottom, visual_mode=True, break_filter=None)
+
+    invalid_elements = []
+
+    for node in PreOrderIter(node_tree[bottom]):
+        name = node.name.split("/")[-1]
+
+        elements = []
+        for child in tree[name]["p"]:
+            elements.append(weights[child])
+
+        if elements[1:] != elements[:-1]:
+            invalid_elements.append(name)
+            # print(name, elements)
+            # print(name, False)
+
+    # parse_tree(raw, parse_filter=invalid_elements)
+
+    print(invalid_elements)
+
+    build_tree(invalid_elements[0], parse_filter=invalid_elements)
+
+    for pre, fill, node in RenderTree(node_tree["uduyfo"]):
+        print("%s%s (%s)" % (pre, node.name, str(weights[node.name.split("/")[-1]])))
+
+    # calculate_balance(invalid_elements[0], visual_mode=True, parse_filter=invalid_elements)
+
+    for element in tree["uduyfo"]["p"]:
+        print(element, tree[element]["w"])
 
 
 main()
