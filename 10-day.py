3fb1ee79c6642f399ecb31086405fd380fe7e19c
capJavert <ante.baric3@gmail.com>
2017-12-10 15:40:41 +0100

    10-day part 2 completed

--- 10-day.py
+++ 10-day.py
@@ -13,25 +13,32 @@ def get_and_prepare_data_string():
 
 
 def main():
-    lengths = get_and_prepare_data_string().split(",")
+    lengths = [ord(i) for i in get_and_prepare_data_string()] + [17, 31, 73, 47, 23]
 
     a = [i for i in range(256)]
     p, skip = 0, 0
 
-    for length in lengths:
-        length = int(length)
+    for i in range(64):
+        for length in lengths:
+            length = int(length)
 
-        if length <= 1:
-            pass
-        else:
-            sub = (a[p:p + length]+a[:length - len(a[p:p + length])])[::-1]
-            a[p:p + length] = sub[:len(a[p:p + length])]
-            a[:length - len(a[p:p + length])] = sub[len(a[p:p + length]):]
+            if length <= 1:
+                pass
+            else:
+                sub = (a[p:p + length]+a[:length - len(a[p:p + length])])[::-1]
+                a[p:p + length] = sub[:len(a[p:p + length])]
+                a[:length - len(a[p:p + length])] = sub[len(a[p:p + length]):]
 
-        p = (p + (length+skip)) % len(a)
-        skip += 1
+            p = (p + (length+skip)) % len(a)
+            skip += 1
 
-    print(a[0]*a[1])
+    dense_hash = []
+    xors = ["^".join([str(i) for i in a[i*16:(i+1)*16]]) for i in range(16)]
+
+    for xor in xors:
+        exec('dense_hash.append(hex('+xor+')[2:].zfill(2))')
+
+    print("".join(dense_hash))
 
 
 main()
