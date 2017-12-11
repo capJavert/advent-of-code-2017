129f93812a7d8e35dffed10c7100042a8f535406
capJavert <ante.baric3@gmail.com>
2017-12-11 21:41:23 +0100

    11-day part 2 completed + reworked part 1 https://www.redblobgames.com/grids/hexagons/#distances

--- 11-day.py
+++ 11-day.py
@@ -1,7 +1,4 @@
 import requests
-from pathfinding.core.diagonal_movement import DiagonalMovement
-from pathfinding.core.grid import Grid
-from pathfinding.finder.a_star import AStarFinder
 
 
 def get_and_prepare_data_string():
@@ -15,65 +12,40 @@ def get_and_prepare_data_string():
     return request.text
 
 
-def cord_to_hex(x, y):
-    breadcrumbs[y][x] = 0
-    breadcrumbs[y+1][x] = 0
-    breadcrumbs[y-1][x] = 0
-    breadcrumbs[y][x+1] = 0
-    breadcrumbs[y][x-1] = 0
-    breadcrumbs[y+1][x+1] = 0
-    breadcrumbs[y-1][x-1] = 0
-
-    breadcrumbs[y+1][x-1] = 1
-    breadcrumbs[y-1][x+1] = 1
-
-
-size = 3100  # this is the lowest approx number that was enough for breadcrumbs path TODO: optimize for step 2
-breadcrumbs = [[0 for _ in range(size)] for _ in range(size)]
-
-
 def main():
     directions = get_and_prepare_data_string().split(",")
 
-    x, y = int(size/2), int(size/2)
-    start = (x, y)
-
-    cord_to_hex(x, y)
+    x, y, z = 0, 0, 0
+    breadcrumbs = []
 
     for direction in directions:
         if "n" == direction:
-            x -= 1
-            y -= 1
+            y += 1
+            z -= 1
 
         if "ne" == direction:
-            y -= 1
+            z -= 1
+            x += 1
 
         if "nw" == direction:
+            y += 1
             x -= 1
 
         if "s" == direction:
-            x += 1
-            y += 1
+            y -= 1
+            z += 1
 
         if "se" == direction:
+            y -= 1
             x += 1
 
         if "sw" == direction:
-            y += 1
+            x -= 1
+            z += 1
 
-        cord_to_hex(x, y)
+        breadcrumbs.append((abs(x) + abs(y) + abs(z)) / 2)
 
-    target = (x, y)
-
-    grid = Grid(matrix=breadcrumbs)
-
-    start = grid.node(start[0], start[1])
-    end = grid.node(target[0], target[1])
-
-    finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
-    path, runs = finder.find_path(start, end, grid)
-
-    print(len(path)-1)
+    print(breadcrumbs[-1], max(breadcrumbs))
 
 
 main()
