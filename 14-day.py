1ae822c45af0125d72c5b04216685126991251e5
capJavert <ante.baric3@gmail.com>
2017-12-14 15:23:09 +0100

    14-day part 2 completed

--- 14-day.py
+++ 14-day.py
@@ -1,3 +1,5 @@
+from heapq import heappop, heappush
+
 
 def knot_hash(data):
     lengths = [ord(i) for i in data] + [17, 31, 73, 47, 23]
@@ -28,15 +30,66 @@ def knot_hash(data):
     return "".join(dense_hash)
 
 
+def maze2graph(maze):
+    height = len(maze)
+    width = len(maze[0]) if height else 0
+    graph = {(i, j): [] for j in range(width) for i in range(height) if not maze[i][j]}
+    for row, col in graph.keys():
+        if row < height - 1 and not maze[row + 1][col]:
+            graph[(row, col)].append(("S", (row + 1, col)))
+            graph[(row + 1, col)].append(("N", (row, col)))
+        if col < width - 1 and not maze[row][col + 1]:
+            graph[(row, col)].append(("E", (row, col + 1)))
+            graph[(row, col + 1)].append(("W", (row, col)))
+    return graph
+
+
+def heuristic(cell, goal):
+    return abs(cell[0] - goal[0]) + abs(cell[1] - goal[1])
+
+
+def find_path_astar(maze, start):
+    goal = (len(maze) - 1, len(maze[0]) - 1)
+    pr_queue = []
+    heappush(pr_queue, (0 + heuristic(start, goal), 0, "", start))
+    visited = set()
+    graph = maze2graph(maze)
+
+    while pr_queue:
+        _, cost, path, current = heappop(pr_queue)
+        if current == goal:
+            return visited
+        if current in visited:
+            continue
+        visited.add(current)
+        # print(current)
+        for direction, neighbour in graph[current]:
+            heappush(pr_queue, (cost + heuristic(neighbour, goal), cost + 1,
+                                path + direction, neighbour))
+    return visited
+
+
 def main():
     puzzle_input = "oundnydw"
 
-    used = 0
+    grid = []
+    groups = []
 
     for i in range(128):
-        used += "".join(format(int(c, 16), '04b') for c in knot_hash("".join([puzzle_input, "-", str(i)]))).count("1")
+        grid.append(
+            list("".join(format(int(c, 16), '04b') for c in knot_hash("".join([puzzle_input, "-", str(i)])))
+                 .replace("1", "X").replace("0", "1").replace("X", "0"))
+        )
+    grid = [[int(square) for square in block] for block in grid]
 
-    print(used)
+    for i, block in enumerate(grid):
+        for j, square in enumerate(block):
+            if any((i, j) in group for group in groups) or square == 1:
+                continue
+
+            groups.append(find_path_astar(grid, (i, j)))
+
+    print(len(groups))
 
 
 main()
