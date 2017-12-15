02643126c087a5270f2b8679b3e25d18835fcf49
capJavert <ante.baric3@gmail.com>
2017-12-15 10:49:43 +0100

    15-day part 2 completed

--- 15-day.py
+++ 15-day.py
@@ -1,10 +1,22 @@
 
 def gen_a(value):
-    return (value*16807) % 2147483647
+    next_value = (value * 16807) % 2147483647
+
+    while next_value % 4 != 0:
+        value = next_value
+        next_value = (value*16807) % 2147483647
+
+    return next_value
 
 
 def gen_b(value):
-    return (value*48271) % 2147483647
+    next_value = (value * 48271) % 2147483647
+
+    while next_value % 8 != 0:
+        value = next_value
+        next_value = (value * 48271) % 2147483647
+
+    return next_value
 
 
 def main():
@@ -12,7 +24,7 @@ def main():
 
     final_countdown = 0
 
-    for i in range(40000000):
+    for i in range(5000000):
         a = gen_a(a)
         b = gen_b(b)
 
