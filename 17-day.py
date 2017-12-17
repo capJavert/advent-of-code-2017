3ce306ba10a4bddbc92c91b15658f1bd2735e2cc
capJavert <ante.baric3@gmail.com>
2017-12-17 15:17:47 +0100

    17-day part 2 completed

--- 17-day.py
+++ 17-day.py
@@ -1,15 +1,19 @@
 
 def main():
     n = 355
-    buffer = [0]
+    buffer = 1
     p = 0
+    resolver = None
 
-    for i in range(1, 2018):
-        p = (p + n) % len(buffer)
-        buffer.insert(p+1, i)
+    for i in range(1, 50000000):
+        p = (p + n) % buffer
+        if p == 0:
+            resolver = i
+
+        buffer += 1
         p += 1
 
-    print(buffer[p+1])
+    print(resolver)
 
 
 main()
